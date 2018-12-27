7c2dd3f76571f1e846fe97f2f8ff31a032e8f5e3
Manddje <kris@mandemaker.info>
2018-12-27 15:06:27 +0100

    Create RemoveAliasFromMailboxes.ps1

---
+++ Powershell/RemoveAliasFromMailboxes.ps1
@@ -0,0 +1,24 @@
+﻿# Get all mailboxes
+$mailboxes = get-mailbox;
+ 
+# Loop through each mailbox
+foreach ($mailbox in $mailboxes) {
+ 
+    $emailaddresses = $mailbox.emailaddresses;
+ 
+    #Loop through each SMTP address found on each mailbox
+    for ($i=0; $i -lt $emailaddresses.count; $i++) {
+        
+        # Change the domain name below to what you want to remove
+        if ($emailaddresses[$i].smtpaddress -like "domein.nl") {
+ 
+            # Remove the unwanted email address
+            $badaddress = $emailaddresses[$i];
+            $emailaddresses = $emailaddresses - $badaddress;
+            $mailbox | set-mailbox -emailaddresses $emailaddresses;
+ 
+        }
+ 
+    }
+ 
+}
